484f70e93c85fad71de2012a83e2cf01e845741a
KatieCodeWitch <71100992+katiekate88@users.noreply.github.com>
2021-01-24 22:52:38 -0500

    Initial commit

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+madlib = f"Coding is so {adj1}! It makes me so {adj2} all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
